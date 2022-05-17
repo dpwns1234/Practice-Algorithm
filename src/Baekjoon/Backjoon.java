4a1edbb6d9a6708872e5ad2a3646b1558cb46dc7
dpwns1234 <dpwns1234@naver.com>
2022-05-17 15:34:32 +0900

    Solve Baekjoon 1138 - Silver2

--- src/Baekjoon/Backjoon.java
+++ src/Baekjoon/Backjoon.java
@@ -1,10 +1,13 @@
 package Baekjoon;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Backjoon {
 	public static void main(String[] args) {
-		solution1();
+		p1138();
 	}
 	
 	// 문제: https://www.acmicpc.net/problem/15829 
@@ -29,5 +32,32 @@ public class Backjoon {
 		}
 		
 		System.out.print(sum);
+	}
+	
+	public static void p1138() {
+		Scanner scr = new Scanner(System.in);
+		int n;
+		n = scr.nextInt();
+		Vector<Integer> list = new Vector<Integer>();
+		
+		// 입력
+		for(int i=0; i<n; i++) {
+			int num = scr.nextInt();
+			list.add(num);
+		}
+		Vector<Integer> answer = new Vector<Integer>(list);
+		
+        int mens = list.size();
+        // 큰 수부터 차례대로 배치
+        for(int i = mens-1; i>=0; i--) {
+        	answer.add(list.get(i), mens);
+        	mens--;
+        }
+		
+        // 출력
+        for(int i=0; i<n; i++) {
+        	System.out.print(answer.get(i) + " ");
+        }
+		
 	}
 }
